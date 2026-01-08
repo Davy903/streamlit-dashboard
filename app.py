32f81e79b76edb8c06c2de433c44259cd0238cf6
Davy903 <he304999@students.ephec.be>
2026-01-08 15:16:40 +0100

    test broker

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 import streamlit as st
+import paho.mqtt.client as mqtt
+import json, time
 
-st.title("Mon premier dashboard Streamlit")
-st.write("Ça marche")
+BROKER = "20.251.215.215"
+TOPIC = "capteurs/esp32"
+
+if "last" not in st.session_state:
+    st.session_state.last = {}
+
+def on_message(client, userdata, msg):
+    st.session_state.last = json.loads(msg.payload.decode())
+
+client = mqtt.Client()
+client.on_message = on_message
+client.connect(BROKER, 1883, 60)
+client.subscribe(TOPIC)
+client.loop_start()
+
+st.title("Live MQTT")
+while True:
+    st.json(st.session_state.last)
+    time.sleep(1)
